a7d335eca99cddbedf21b9d496d3806799b0fa7f
Gustavo Henrique <48604457+Tavinhuu@users.noreply.github.com>
2024-11-19 09:28:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int nav;
+
 void jogar(){
 
 }
@@ -27,4 +29,17 @@ void mostrarInfo(){
 
 int main(void){
     
+    printf("Escolha a dificuldade\n");
+    printf("Nível 1 (Fácil): Valores entre 0 e 10.\n Nível 2 (Médio): Valores entre 0 e 100.\n Nível 3 (Difícil): Valores entre 0 e 1000.\n Nível 4 (Insano): Valores entre 0 e 10000.");
+    switch (nav)
+    {
+    case 1:
+
+        break;
+    
+    default:
+        break;
+    }
+
+
 }
