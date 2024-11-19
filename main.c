b75912793f473066f7ade8809772c5b342680efb
Gustavo Henrique <48604457+Tavinhuu@users.noreply.github.com>
2024-11-19 09:26:10 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void jogar(){
+
+}
+
+void adicao(){
+
+}
+
+void multiplicacao(){
+    
+}
+
+void divisao(){
+    
+}
+
+void subtracao(){
+    
+}
+
+void mostrarInfo(){
+    
+}
+
+
+int main(void){
+    
+}
