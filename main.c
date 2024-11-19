9d88a092fd353200093ed1ea72c21357b71be294
Gustavo Henrique <48604457+Tavinhuu@users.noreply.github.com>
2024-11-19 10:15:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include <time.h>
 
-int dificuldade;
+
+typedef struct { 
+    int dificuldade; 
+    int valor1; 
+    int valor2; 
+    int operacao; 
+    int resultado; 
+} Calcular;
+
+Calcular calc1;
+
 
 int nav;
 
 void jogar(){
 
     srand(time(NULL));
-    int operacao = rand() % 4;
+    calc1.operacao = rand() % 4;
 
 
-    if(operacao == 0) {
+    if(calc1.operacao == 0) {
         adicao();
     }
 
-    if (operacao == 1){
+    if (calc1.operacao == 1){
         multiplicacao();
     }
 
-    if (operacao == 2){
+    if (calc1.operacao == 2){
         divisao();
     }
 
-    if (operacao == 3){
+    if (calc1.operacao == 3){
         subtracao();
     }
 
@@ -31,38 +41,36 @@ void jogar(){
 }
 
 void adicao(){
-    printf("Operacao de adicao\n");
 
-    if(dificuldade == 1){
-        printf("Dificuldade facil");
+    if(calc1.dificuldade == 1){
+        mostrarInfo();
+
     }
-    if(dificuldade == 2){
-        printf("Dificuldade medio");
+    if(calc1.dificuldade == 2){
+        mostrarInfo();
     }
-    if(dificuldade == 3){
-        printf("Dificuldade dificil");
+    if(calc1.dificuldade == 3){
+        mostrarInfo();
     }
-    if(dificuldade == 4){
-        printf("Dificuldade insano");
+    if(calc1.dificuldade == 4){
+        mostrarInfo();
     }
 
 }
 
 void multiplicacao(){
-    printf("Operacao de multiplicacao\n");
 
-
-    if(dificuldade == 1){
-        printf("Dificuldade facil");
+    if(calc1.dificuldade == 1){
+        mostrarInfo();
     }
-    if(dificuldade == 2){
-        printf("Dificuldade medio");
+    if(calc1.dificuldade == 2){
+        mostrarInfo();
     }
-    if(dificuldade == 3){
-        printf("Dificuldade dificil");
+    if(calc1.dificuldade == 3){
+        mostrarInfo();
     }
-    if(dificuldade == 4){
-        printf("Dificuldade insano");
+    if(calc1.dificuldade == 4){
+        mostrarInfo();
     }
 
 
@@ -70,19 +78,17 @@ void multiplicacao(){
 
 void divisao(){
 
-    printf("Operacao de divisao\n");
-
-    if(dificuldade == 1){
-        printf("Dificuldade facil");
+    if(calc1.dificuldade == 1){
+        mostrarInfo();
     }
-    if(dificuldade == 2){
-        printf("Dificuldade medio");
+    if(calc1.dificuldade == 2){
+        mostrarInfo();
     }
-    if(dificuldade == 3){
-        printf("Dificuldade dificil");
+    if(calc1.dificuldade == 3){
+        mostrarInfo();
     }
-    if(dificuldade == 4){
-        printf("Dificuldade insano");
+    if(calc1.dificuldade == 4){
+        mostrarInfo();
     }
 }
 
@@ -90,23 +96,62 @@ void subtracao(){
     
     printf("Operacao de subtracao\n");
 
-    if(dificuldade == 1){
-        printf("Dificuldade facil");
+    if(calc1.dificuldade == 1){
+        mostrarInfo();
     }
-    if(dificuldade == 2){
-        printf("Dificuldade medio");
+    if(calc1.dificuldade == 2){
+        mostrarInfo();
     }
-    if(dificuldade == 3){
-        printf("Dificuldade dificil");
+    if(calc1.dificuldade == 3){
+        mostrarInfo();
     }
-    if(dificuldade == 4){
-        printf("Dificuldade insano");
+    if(calc1.dificuldade == 4){
+        mostrarInfo();
     }
 
 }
 
+
 void mostrarInfo(){
     
+    if(calc1.dificuldade==1){
+        printf("\t\t\t _________________________________________________\n");
+	    printf("\t\t\t|                                                 |\n");
+	    printf("\t\t\t|                    NIVEL FACIL                  |\n");
+	    printf("\t\t\t| - - - - - - - - - - - - - - - - - - - - - - - - |\n");
+    }
+    else if(calc1.dificuldade==2){
+        printf("\t\t\t _________________________________________________\n");
+	    printf("\t\t\t|                                                 |\n");
+	    printf("\t\t\t|                    NIVEL MEDIO                  |\n");
+	    printf("\t\t\t| - - - - - - - - - - - - - - - - - - - - - - - - |\n");
+    }
+    else if(calc1.dificuldade==3){
+        printf("\t\t\t _________________________________________________\n");
+	    printf("\t\t\t|                                                 |\n");
+	    printf("\t\t\t|                  NIVEL DIFICIL                  |\n");
+	    printf("\t\t\t| - - - - - - - - - - - - - - - - - - - - - - - - |\n");
+    }
+    else{
+        printf("\t\t\t _________________________________________________\n");
+	    printf("\t\t\t|                                                 |\n");
+	    printf("\t\t\t|                  NIVEL INSANO                   |\n");
+	    printf("\t\t\t| - - - - - - - - - - - - - - - - - - - - - - - - |\n");
+    }
+        printf("\t\t\t| Valores:%d %d                                   |\n");
+
+    if(calc1.operacao==1){
+        printf("\t\t\t| Operacao: Soma                                  |\n");
+    }
+    else if(calc1.operacao==2){
+        printf("\t\t\t| Operacao: Subtracao                             |\n");
+    }
+    else if(calc1.operacao==3){
+        printf("\t\t\t| Operacao: Multiplicacao                         |\n");
+    }
+    else{
+        printf("\t\t\t| Operacao: Divisao                               |\n");
+    }
 }
 
 
@@ -114,25 +159,32 @@ int main(void){
     
 
 
-    printf("Escolha a dificuldade\n");
-    printf("Nível 1 (Facil): Valores entre 0 e 10.\n Nível 2 (Medio): Valores entre 0 e 100.\n Nível 3 (Dificil): Valores entre 0 e 1000.\n Nível 4 (Insano): Valores entre 0 e 10000.\n\n");
+    printf("\t\t\t _________________________________________________\n");
+	printf("\t\t\t|                                                 |\n");
+	printf("\t\t\t|               ESCOLHA A DIFICULDADE             |\n");
+	printf("\t\t\t| - - - - - - - - - - - - - - - - - - - - - - - - |\n");
+    printf("\t\t\t| Nivel 1 (Facil): Valores entre 0 e 10.          |\n");
+    printf("\t\t\t| Nivel 2 (Facil): Valores entre 0 e 100.         |\n");
+    printf("\t\t\t| Nivel 3 (Dificil): Valores entre 0 e 1000.      |\n");
+    printf("\t\t\t| Nivel 4 (Insano): Valores entre 0 e 10000.      |\n");
+    printf("\t\t\t _________________________________________________\n");
     scanf("%d", &nav);
     switch (nav)
     {
     case 1:
-        dificuldade = 1;
+        calc1.dificuldade = 1;
         jogar();
         break;
     case 2:
-        dificuldade = 2;
+        calc1.dificuldade = 2;
         jogar();
         break;
     case 3:
-        dificuldade = 3;
+        calc1.dificuldade = 3;
         jogar();
         break;
     case 4:
-        dificuldade = 4;
+        calc1.dificuldade = 4;
         jogar();
         break;
     default:
